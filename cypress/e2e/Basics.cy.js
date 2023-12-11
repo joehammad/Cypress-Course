8eca4c09b33eae14606ce9bcaeb2e0c759ebbd5c
joehammad <joehammad4@gmail.com>
2023-12-11 16:40:53 +0200

    new test

--- cypress/e2e/Basics.cy.js
+++ cypress/e2e/Basics.cy.js
@@ -2,20 +2,5 @@ describe("Basic Test",()=> {
     beforeEach('visit base URL', ()=>{
      cy.visit('https://para.testar.org/parabank/index.htm;jsessionid=D8ED8A472732ABEE3365314242085787')
     })
-    it('check logo',()=> { 
-      cy.get(".logo").should('be.visible')
-    })
-    it("click on admin page", ()=> {
-        cy.contains("Admin Page").click()
-    })
-    it('Enter Username and password', ()=>{
-
-        
-        cy.get('[name="username"]').type('test')
-        cy.get('[name="password"]').type('123')
-    })
-    it ('click on read more', ()=>{
-        cy.get('.more').contains("Read More").click() 
-    })
   
 })
